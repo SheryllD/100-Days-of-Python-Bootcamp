3dcffc5eb2881a4dc8e19111ac1ecc9ff602b011
Sheryll Dumapal <sheryllhhs@outlook.com>
2024-05-29 23:16:28 +0200

    save

---
+++ Day_002/002_Tip_Calculator.py
@@ -0,0 +1,5 @@
+# If the bill was 150 euro, split between 5 people, with 12% tip. 
+# Each person should pa *(150 /5) * 1.12 = 33.6
+# Round the result to 2 decimal places = 33.60
+
+print("welcome to the tip calculatur!")
