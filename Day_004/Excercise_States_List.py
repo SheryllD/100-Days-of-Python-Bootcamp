5a5a22643711cf7686c91d816d45d92bf643fb5e
Sheryll Dumapal <sheryllhhs@outlook.com>
2024-08-20 16:49:37 +0200

    Completed Task

--- Day_004/Excercise_States_List.py
+++ Day_004/Excercise_States_List.py
@@ -1,9 +1,15 @@
-states_of_america = ["Delaware", "Pennsylvania", "Alabama", "California", "Indiana"]
+#states_of_america = ["Delaware", "Pennsylvania", "Alabama", "California", "Indiana"]
 
 #print(states_of_america[-2])
 
-states_of_america[1] = "Penny"
+#states_of_america[1] = "Penny"
 #print(states_of_america[1])
 #states_of_america.append("Angelaland")
-states_of_america.extend(["Angelaland", "Jack Bauer Land"])
-print(states_of_america)
+#states_of_america.extend(["Angelaland", "Jack Bauer Land"])
+#print(states_of_america)
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+num_of_states = len(states_of_america) #50 -> 49
+
+print(states_of_america[num_of_states - 1])
