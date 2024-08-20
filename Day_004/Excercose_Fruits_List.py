4bba09573524fb0a56b7c2737ad310cd7edb6293
Sheryll Dumapal <sheryllhhs@outlook.com>
2024-08-20 16:54:48 +0200

    Completed Task with Fruits

---
+++ Day_004/Excercose_Fruits_List.py
@@ -0,0 +1,6 @@
+#dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
